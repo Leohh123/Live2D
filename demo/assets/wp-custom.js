4e84844b23dd53a0e3800eb15cdebf0740ed8ca7
Leohh <948750310@qq.com>
2019-06-15 15:56:28 +0800

    v1.0.1

--- demo/assets/wp-custom.js
+++ demo/assets/wp-custom.js
@@ -1,5 +1,5 @@
 try {
-    var path = "//cdn.jsdelivr.net/gh/Leohh123/Live2D@v1.0.0/demo/";
+    var path = "//cdn.jsdelivr.net/gh/Leohh123/Live2D@v1.0.1/demo/";
     $.ajax({url: path + "assets/jquery-ui.min.js?v=1.12.1", dataType:"script", cache: true, success: function() {
         $("<link>").attr({href: path + "assets/waifu.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
         $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
